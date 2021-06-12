b5eb369f30c32a8164f4dcfe267c05d227428de0
Cleyton Mendes <cleyton.mendest@gmail.com>
2021-06-11 23:25:55 -0300

    atualização event click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
-function show(el){
-    
-}
+const span = document.querySelectorAll('.span');
+
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    for(let i of span){
+        const faq1 = i.parentElement;
+        faq1.childNodes[4].classList.remove('active');
+        faq1.childNodes[2].style.transform = "rotate(0deg)";
+
+        if(i === el){
+            faq1.childNodes[4].classList.add('active');
+            faq1.childNodes[2].style.transform = "rotate(180deg)";
+            return
+        }
+    }
+})
